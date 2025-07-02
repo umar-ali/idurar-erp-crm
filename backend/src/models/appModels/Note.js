950e1c28133f211bc6d390de668402f9600f2e96
umar <aligani619@gmail.com>
2025-07-02 13:41:24 +0530

    Add schema for Query Management

---
+++ backend/src/models/appModels/Note.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    query: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Query',
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Client",
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongoose.model('Note', noteSchema)
---
+++ backend/src/models/appModels/Query.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const querySchema = new Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['Open', 'InProgress', 'Closed'],
+        default: 'Open'
+    },
+    resolution: {
+        type: String,
+        trim: true,
+        maxlength: 1000 
+    }
+}, {
+    timestamps: true 
+});
+
+
+module.exports = mongoose.model('Query', querySchema)
