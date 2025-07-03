0eecee2b6ec30896c02accaf60a8136601718ff4
umar <aligani619@gmail.com>
2025-07-04 00:44:56 +0530

    Add update method to controller

--- backend/src/controllers/appControllers/queryController/index.js
+++ backend/src/controllers/appControllers/queryController/index.js
@@ -3,12 +3,14 @@ const methods = createCRUDController('Query');
 
 
 const create = require("./create");
+const update = require("./update");
 const {readOne, paginatedList} = require("./read")
 const {create: addNote, remove: removeNote} = require("./queryNotes")
 
 methods.create = create;
 methods.read = readOne;
 methods.list = paginatedList;
+methods.update = update;
 methods.addNote = addNote;
 methods.removeNote = removeNote;
 
